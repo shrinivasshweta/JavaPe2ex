f085be1c1d3ff9ce05e5297967b5ae004c48df55
shrinivasshweta <41103798+shrinivasshweta@users.noreply.github.com>
2018-08-06 10:44:49 +0530

    Test Case for Student Grade

--- TestPE/src/test/java/Com/Stackroute/Demo/PE2/TestCs3.java
+++ TestPE/src/test/java/Com/Stackroute/Demo/PE2/TestCs3.java
@@ -1,5 +1,35 @@
 package Com.Stackroute.Demo.PE2;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-public class TestCs3 {
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+	public class MemVarTest {
+	
+	
+    private static MemVarTest obj;
+    
+    @BeforeClass
+    public static void setup() {
+        obj = new MemVarTest();
+    }
+    
+    @AfterClass
+    public static void teardown() {
+        obj = null;
+    }
+    @Test
+    public void getMemVarTest() {
+        MemberVariable ob = new MemberVariable();
+        assertArrayEquals(new Object[] {"Harry Potter", 30 ,2500.3}, ob.getMemVar("Harry Potter", 30 ,2500.3));
+        
+        assertNotNull(ob.getMemVar("Harry Potter", 30 ,2500.3));
+
+        assertNull(null, ob.getMemVar(null, 0, 0));
+    }
 }
